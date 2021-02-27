c1511e7c311a30f0faeae015b318ec500df889f0
moonjjj <mjesun1@naver.com>
2021-02-27 12:09:21 +0900

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,12 @@
-var m = window.matchMedia("screen and (max-width:757px)")
-
 
 document.addEventListener('DOMContentLoaded',function(){
     console.log("loading완료");
-    console.log(m.media);
-    console.log(m.matches);
     phone();
 });
 
 function phone(){
-    if(m.matches==true){
+    var mo = window.matchMedia("screen and (max-width:757px)")
+    if(mo.matches==true){
         document.getElementById("bg-img").src = "/bg-phone.06e8c994.png";
         console.log("phone");
     }else{

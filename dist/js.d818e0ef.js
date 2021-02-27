6051618e05d90c358c598467408c5e6e3aa56c5b
moonjjj <mjesun1@naver.com>
2021-02-27 12:18:22 +0900

    c

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -119,15 +119,14 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"src/js/index.js":[function(require,module,exports) {
 document.addEventListener('DOMContentLoaded', function () {
-  console.log("loading완료");
-  phone();
+  console.log("loading완료"); // phone();
 });
 
 function phone() {
   var mo = window.matchMedia("screen and (max-width:757px)");
 
   if (mo.matches == true) {
-    document.getElementById("bg-img").src = "src/img/bg-phone.png";
+    document.getElementById("bg-img").src = "/bg-phone.06e8c994.png";
     console.log("phone");
   } else {
     console.log("tablet or desktop");
